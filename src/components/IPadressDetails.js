fd5ded5d921626c9a6e1c9faec29db5ce2db5611
OSHIN <izkp44@gmail.com>
2023-04-29 01:24:22 +0700

    import Detail

--- src/components/IPadressDetails.js
+++ src/components/IPadressDetails.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import Detail from './Detail'
 
-function IPadressDetails() {
-  return <div>IPadressDetails</div>
+function IPadressDetails(props) {
+  // const {ip_address, location, time_zone, isp} = props
+  return (
+    <div className="bg-cyan-400 max-w-[1110px] flex  max-h-[161px] py-[38px]">
+      <Detail about="ip address" info="192.212.174.101" />
+      <Detail about="location" info="Brooklyn, NY 10001" />
+      <Detail about="time zone" info="UTC-5:00" />
+      <Detail about="isp" info="SpaceX Starlink" />
+    </div>
+  )
 }
 
 export default IPadressDetails
