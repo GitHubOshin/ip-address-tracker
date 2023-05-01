6d350b8a9735ec26f8dcf9797405a9d410565171
OSHIN <izkp44@gmail.com>
2023-05-01 17:35:05 +0700

    Add ipAddressDetails in store

--- src/app/stores.js
+++ src/app/stores.js
@@ -1,1 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import ipAddressDetailsReducer from '../slices.js/ipAddressDetailsSlice'
+
+export const store = configureStore({
+  reducer: {
+    ipAddressDetails: ipAddressDetailsReducer
+  }
+})
