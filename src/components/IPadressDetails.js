33d8c4e52d8426a47e39690da7bfe3fe0851d610
OSHIN <izkp44@gmail.com>
2023-05-01 17:40:28 +0700

    Get props

--- src/components/IPadressDetails.js
+++ src/components/IPadressDetails.js
@@ -1,13 +1,13 @@
 import Detail from './Detail'
 
 function IPadressDetails(props) {
-  // const {ip_address, location, time_zone, isp} = props
+  const { ip_address, location, time_zone, isp } = props
   return (
     <div className="bg-cyan-400 max-w-[1110px] flex  max-h-[161px] py-[38px]">
-      <Detail about="ip address" info="192.212.174.101" />
-      <Detail about="location" info="Brooklyn, NY 10001" />
-      <Detail about="time zone" info="UTC-5:00" />
-      <Detail about="isp" info="SpaceX Starlink" />
+      <Detail about="ip address" info={ip_address} />
+      <Detail about="location" info={location} />
+      <Detail about="time zone" info={`UTC ${time_zone}`} />
+      <Detail about="isp" info={isp} />
     </div>
   )
 }
