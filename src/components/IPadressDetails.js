20a52fa9b11da253f85b5d02ddfedd84db1cf5c8
OSHIN <izkp44@gmail.com>
2023-05-06 23:48:21 +0700

    Update IPadressDetails.js

--- src/components/IPadressDetails.js
+++ src/components/IPadressDetails.js
@@ -3,7 +3,7 @@ import Detail from './Detail'
 function IPadressDetails(props) {
   const { ip_address, location, time_zone, isp } = props
   return (
-    <div className="bg-cyan-400 max-w-[1110px] flex  max-h-[161px] py-[38px]">
+    <div className="bg-white rounded-xl max-w-[1110px] flex  max-h-[161px] shadow-2xl py-[38px]">
       <Detail about="ip address" info={ip_address} />
       <Detail about="location" info={location} />
       <Detail about="time zone" info={`UTC ${time_zone}`} />
