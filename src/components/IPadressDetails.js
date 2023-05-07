bc2ff6576fcb47728a18712853b00252aeac3f6f
OSHIN <izkp44@gmail.com>
2023-05-07 23:34:58 +0700

    Add isLoading

--- src/components/IPadressDetails.js
+++ src/components/IPadressDetails.js
@@ -1,14 +1,22 @@
 import Detail from './Detail'
 
 function IPadressDetails(props) {
-  const { ip_address, location, time_zone, isp } = props
+  const { ip_address, location, time_zone, isp, isLoading } = props
   return (
-    <div className="bg-white rounded-xl max-w-[1110px] flex  max-h-[161px] shadow-2xl py-[38px]">
-      <Detail about="ip address" info={ip_address} />
-      <Detail about="location" info={location} />
-      <Detail about="time zone" info={`UTC ${time_zone}`} />
-      <Detail about="isp" info={isp} />
-    </div>
+    <>
+      {isLoading ? (
+        <div className="bg-white rounded-xl w-full h-full max-w-[1110px] text-center  max-h-[161px] shadow-2xl py-[38px]">
+          Loading IP Address...
+        </div>
+      ) : (
+        <div className="bg-white rounded-xl max-w-[1110px] flex  max-h-[161px] shadow-2xl py-[38px]">
+          <Detail about="ip address" info={ip_address} />
+          <Detail about="location" info={location} />
+          <Detail about="time zone" info={`UTC ${time_zone}`} />
+          <Detail about="isp" info={isp} />
+        </div>
+      )}
+    </>
   )
 }
 
