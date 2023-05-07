535602746070f086b80c4815495062590032e343
OSHIN <izkp44@gmail.com>
2023-05-07 23:32:22 +0700

    Update stores.js

--- src/app/stores.js
+++ src/app/stores.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import ipAddressDetailsReducer from '../slices/ipAddressDetailsSlice'
 import ipAddressReducer from '../slices/ipAddressSlice'
+import isLoadingIpReducer from '../slices/isLoadingIP'
 
 export const store = configureStore({
   reducer: {
     ipAddressDetails: ipAddressDetailsReducer,
-    ipAddress: ipAddressReducer
+    ipAddress: ipAddressReducer,
+    isLoadingIP: isLoadingIpReducer
   }
 })
