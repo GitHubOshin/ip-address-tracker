f04bb178ee3dc30ae306492bc71d702d2ed6c2cb
OSHIN <izkp44@gmail.com>
2023-05-08 18:04:29 +0700

    Add errorMessageSlice in store

--- src/app/stores.js
+++ src/app/stores.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import ipAddressDetailsReducer from '../slices/ipAddressDetailsSlice'
 import ipAddressReducer from '../slices/ipAddressSlice'
-import isLoadingIpReducer from '../slices/isLoadingIP'
+import isLoadingIpReducer from '../slices/isLoadingIpSlice'
+import errorMessageReducer from '../slices/errorMessageSlice'
 
 export const store = configureStore({
   reducer: {
     ipAddressDetails: ipAddressDetailsReducer,
     ipAddress: ipAddressReducer,
-    isLoadingIP: isLoadingIpReducer
+    isLoadingIP: isLoadingIpReducer,
+    errorMessage: errorMessageReducer
   }
 })
