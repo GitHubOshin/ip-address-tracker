b60c78ac8a52891a6c8301c28b830fdce1047b5d
OSHIN <izkp44@gmail.com>
2023-05-08 19:09:40 +0700

    Get errorMessage

--- src/components/IPadressDetails.js
+++ src/components/IPadressDetails.js
@@ -1,12 +1,17 @@
 import Detail from './Detail'
 
 function IPadressDetails(props) {
-  const { ip_address, location, time_zone, isp, isLoading } = props
+  const { ip_address, location, time_zone, isp, isLoading, errorMessage } =
+    props
   return (
     <>
       {isLoading ? (
         <div className="bg-white rounded-xl w-full h-full max-w-[1110px] text-center  max-h-[161px] shadow-2xl py-[38px]">
           Loading IP Address...
+        </div>
+      ) : errorMessage !== '' ? (
+        <div className="bg-white text-red-800 rounded-xl w-full h-full max-w-[1110px] text-center  max-h-[161px] shadow-2xl py-[38px]">
+          {errorMessage}
         </div>
       ) : (
         <div className="bg-white rounded-xl max-w-[1110px] flex  max-h-[161px] shadow-2xl py-[38px]">
